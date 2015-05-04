b29ab99e30066dc15b54b8f3e8eaba918c64b5d6
Diogo Ferreira <diogo.rosas.ferreira@gmail.com>
2015-05-04 16:21:02 -0300

    Layout e mensagens

--- cw-persistence/src/main/resources/META-INF/database/setup_database.sql
+++ cw-persistence/src/main/resources/META-INF/database/setup_database.sql
@@ -1,4 +1,4 @@
 ---
 -- Inserção de usuarios padrões
 ---
-INSERT INTO "user" (id, email, name, password) VALUES (1, 'diogo.rosas.ferreira@gmail.com', 'Diogo Rosas Ferreira', '4PaBNNKdwybRFd5Mj6uHAKPEsAI=');
+INSERT INTO "user" (id, email, name, password) VALUES (1, 'admin@admin.com', 'Admin', 'admin');
